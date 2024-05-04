8574a3f8e0cdae071c52ae6b7e28b50a7b7fd2f8
satbani <157083938+satbani@users.noreply.github.com>
2024-05-04 14:08:37 -0400

    Update MathQuestionable.java

--- src/MathQuestionable.java
+++ src/MathQuestionable.java
@@ -1,10 +1,12 @@
-//Replace this interface code with your solution to the Chapter 12 zyLab
-
 import java.util.Random;
 
 public interface MathQuestionable {
     Random RANDOM = new Random(5); //DO NOT CHANGE THIS LINE
-
-    // TODO: Add properties and method headers
-
+    int MAX_SMALL = 25;
+    int MAX_LARGE = 50;
+    int MAX_MULTIPLE = 12;
+    int MAX_BASE = 12;
+    int MAX_EXPONENT = 4;
+    public String getQuestion();
+    public int getCorrectAnswer();
 }
